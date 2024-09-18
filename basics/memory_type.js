01f49cf4f44e36adfa32075fe12fe521f500ed62
kartik82006 <66481305+kartik82006@users.noreply.github.com>
2024-09-18 20:25:00 +0000

    Types of memory and datatype

---
+++ basics/memory_type.js
@@ -0,0 +1,5 @@
+//Stack (Primitive datatype:- string,number,bool,undefined,null,symbol,bigInt)
+//value is copied when used by another integer
+
+//Heap(Non-Primitive datatype :- object,functions,array)
+//value called by refernce
