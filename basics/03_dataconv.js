f8546f5f9e8fc5a6d1719c712615cdf9446637d2
kartik82006 <66481305+kartik82006@users.noreply.github.com>
2024-09-18 19:42:51 +0000

    Prefix,postfix operator and add of str and number

--- basics/03_dataconv.js
+++ basics/03_dataconv.js
@@ -1,8 +1,28 @@
-let isLoggedIn = " "
-console.log(typeof isLoggedIn);
+// let isLoggedIn = " "
+// console.log(typeof isLoggedIn);
 
-let boolIsLoggedIn = Boolean(isLoggedIn)
-console.log(boolIsLoggedIn);
+// let boolIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boolIsLoggedIn);
 
+//****Conversion Operations *****//
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 ="Hello"
+let str2 ="Kartik"
+let str3 = str1+str2
+console.log(str3)
+
+//Addition of String and NUmber
+// console.log(1+"2");
+// console.log("1"+2+1); //Auto converts rest to string to get 121
+// console.log(1+2+"1")  //First add numbers then convert to string to add and get 31
+
+
+let val = 10;
+let y = val++; //postfix value
+let x = ++val; //prefix value operatoe
+console.table([y,x,val])
 
 
